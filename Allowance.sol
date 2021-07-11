03adb211dba1fcdf7b7be76a90c00e632817e43a
Eduardo W. da Cunha <eduardowestc@gmail.com>
2021-07-11 12:54:14 -0300

    Update Allowance.sol

--- Allowance.sol
+++ Allowance.sol
@@ -1,39 +1,57 @@
-pragma solidity ^0.8.4;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-import "./Allowance.sol";
-
-contract SharedWallet is Allowance {
+contract Allowance is Ownable {
     
-    event moneySent(address indexed _toWhom, uint _amount);
-    event moneyReceived(address indexed _fromWho, uint _amount);
-    
-    function renounceOwnership() public view override onlyOwner {
-        revert("Can not renounceOwnership here!");
+    struct Action {
+        int Value;
+        uint timestamps;
     }
     
-    function seeContractBalance() public view returns(uint){
-        return address(this).balance;
+    struct Wallet {
+        mapping(uint => Action) payments_withdrawals;
+        uint TotalBalance;
+        uint numActions;
     }
     
-    function withdrawMoney(address payable _to, uint _amount) public OwnerOrAllowed(_amount) {
-        require(_amount <= address(this).balance, "Contract does not have enough funds.");
-        emit moneySent(_to, _amount);
-        if (!isOwner()){
-            reduceAllowance(_to, _amount);
-        }
-        
-        _to.transfer(_amount);
+    mapping(address => Wallet) walletBalance;
+    
+    event allowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
+    
+    modifier OwnerOrAllowed(uint _amount) {
+        require(isOwner() || walletBalance[msg.sender].TotalBalance >= _amount,"You do not have permission.");
+        _;
     }
     
-    function receiveMoney() internal {
-        emit moneyReceived(msg.sender, msg.value);
-        if (!isOwner()){
-            receivePayment(msg.sender,msg.value);
-        }
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
     }
     
-    receive() external payable {
-        receiveMoney();
+    function setAllowance(address _to, uint _amount) public payable onlyOwner {
+        emit allowanceChanged(_to, msg.sender, walletBalance[_to].TotalBalance, _amount);
+        walletBalance[_to].TotalBalance = _amount;
     }
     
+    function increaseAllowance(address _to, uint _amount) public payable onlyOwner {
+        emit allowanceChanged(_to, msg.sender, walletBalance[_to].TotalBalance, walletBalance[_to].TotalBalance + _amount);
+        receivePayment(_to,_amount);
+    }
+    
+    function receivePayment(address _to, uint _amount) internal{
+        walletBalance[_to].TotalBalance += _amount;
+        walletBalance[_to].payments_withdrawals[walletBalance[_to].numActions].Value = int(_amount);
+        walletBalance[_to].payments_withdrawals[walletBalance[_to].numActions].timestamps = block.timestamp;
+        walletBalance[_to].numActions += 1;
+    }
+    
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit allowanceChanged(_who, msg.sender, walletBalance[_who].TotalBalance, walletBalance[_who].TotalBalance - _amount);
+        walletBalance[_who].TotalBalance -= _amount;
+        walletBalance[_who].payments_withdrawals[walletBalance[_who].numActions].Value = -int(_amount);
+        walletBalance[_who].payments_withdrawals[walletBalance[_who].numActions].timestamps = block.timestamp;
+        walletBalance[_who].numActions += 1;
+    }
+    
+    function seeBalance(address _address) public view returns(uint) {
+        return walletBalance[_address].TotalBalance;
+    }
 }
